3663697c18bb65a5b12c27c400ed4ff6fc5fb489
Filip Bianga <filipbianga@pop-os.localdomain>
2021-12-18 00:13:27 +0100

    Lab3 Test REST API - First version

---
+++ LAB3/src/main/java/com/example/LAB3/PatientRecordRepository.java
@@ -0,0 +1,8 @@
+package com.example.LAB3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRecordRepository extends JpaRepository<PatientRecord, Long> {
+}
